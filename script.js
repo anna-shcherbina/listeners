0562367cc5b758689cc187322ac950d274bfc4b6
Anna Shcherbinina <nyusya_belous@mail.ru>
2024-01-21 16:20:48 +0700

    done lesson 11

--- script.js
+++ script.js
@@ -1,3 +1,39 @@
+'use strict'
+
+const textInput = document.getElementById('text');
+const btn = document.getElementById('btn');
+const square = document.querySelector('#square');
+const range = document.getElementById('range');
+const spanRange = document.getElementById('range-span');
+const btnSquare = document.querySelector('#e_btn');
+const circle = document.querySelector('#circle');
+
+const getSquareColor = function () {
+
+    square.style.backgroundColor = textInput.value;
+}
+
+btnSquare.style.display = 'none';
+
+const changeСircle = function (event) {
+
+    spanRange.textContent = event.target.value;
+    circle.style.height = event.target.value + '%';
+    circle.style.width = event.target.value + '%';
+    console.dir(event.type);
+
+}
+
+btn.addEventListener('click', getSquareColor);
+range.addEventListener('input', changeСircle);
+range.addEventListener('change', changeСircle);
+
+console.dir(circle);
+
+
+
+
+
 
 
 
